1da1185ac7598324eaffe7b2c3f809a2a62726ac
retroxz <zzxee666@gmail.com>
2021-04-14 16:06:29 +0800

    初始化VuePress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+// .vuepress/config.js
+module.exports = {
+    theme: 'yuu',
+    themeConfig: {
+        navbar: false,
+        sidebar: 'auto',
+        yuu: {
+            defaultDarkTheme: true,
+        }
+    }
+}
